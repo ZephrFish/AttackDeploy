a7d2d0e079b933e41400502468b273a25387c310
ZephrFish <ZephrFish@users.noreply.github.com>
2017-05-30 10:32:58 +0100

    Update AttackDeploy.sh

--- AttackDeploy.sh
+++ AttackDeploy.sh
@@ -95,7 +95,7 @@ git clone https://github.com/robertdavidgraham/masscan
 # Web Server & SSL Setup
 apt install nginx -y
 # wget https://raw.githubusercontent.com/ZephrFish/AttackDeploy/master/NginxConfig -o /etc/nginx/sites-enabled/default
-# wget https://raw.githubusercontent.com/ZephrFish/AttackDeploy/master/sslsnip-siteHere -o /etc/nginx/snippets/ssl-SITEHERE.conf
+# wget https://raw.githubusercontent.com/ZephrFish/AttackDeploy/master/sslsnip-SITEHERE -o /etc/nginx/snippets/ssl-SITEHERE.conf
 
 # Setup All the Fucking Things
 # WIP
